025a7748c9b4d7bdf9f9b4ad8536f06cb32c3705
Aditya R B <bhusanuraditya@gmail.com>
2024-08-28 11:32:41 +0530

    commit-2

--- Tokenization_build.c
+++ Tokenization_build.c
@@ -12,43 +12,43 @@ int main()
         switch (i)
         {
         case 0:
-            printf("<0>\t %s\n", token);
+            printf("<0>\t%s\n", token);
             break;
         case 1:
-            printf("UTC time, the format is hhmmss.sss: %s\n", token);
+            printf("UTC time, the format is hhmmss.sss:\t%s\n", token);
             break;
         case 2:
-            printf("Positioning status, A=effective positioning, V=invalid positioning: %s\n", token);
+            printf("Positioning status, A=effective positioning, V=invalid positioning:\t%s\n", token);
             break;
         case 3:
-            printf("Latitude, the format is ddmm.mmmmmmm: %s\n", token);
+            printf("Latitude, the format is ddmm.mmmmmmm:\t%s\n", token);
             break;
         case 4:
-            printf("Latitude hemisphere, N or S (north latitude or south latitude): %s\n", token);
+            printf("Latitude hemisphere, N or S (north latitude or south latitude):\t%s\n", token);
             break;
         case 5:
-            printf("Longitude, the format is dddmm.mmmmmmm: %s\n", token);
+            printf("Longitude, the format is dddmm.mmmmmmm:\t%s\n", token);
             break;
         case 6:
-            printf("Longitude hemisphere, E or W (east longitude or west longitude): %s\n", token);
+            printf("Longitude hemisphere, E or W (east longitude or west longitude):\t%s\n", token);
             break;
         case 7:
-            printf("Ground speed: %s\n", token);
+            printf("Ground speed:\t%s\n", token);
             break;
         case 8:
-            printf("Ground heading (take true north as the reference datum): %s\n", token);
+            printf("Ground heading (take true north as the reference datum):\t%s\n", token);
             break;
         case 9:
-            printf("UTC date, the format is ddmmyy (day, month, year): %s\n", token);
+            printf("UTC date, the format is ddmmyy (day, month, year):\t%s\n", token);
             break;
         case 10:
-            printf("Magnetic declination (000.0~180.0 degrees): %s\n", token);
+            printf("Magnetic declination (000.0~180.0 degrees):\t%s\n", token);
             break;
         case 11:
-            printf("Magnetic declination direction, E (east) or W (west): %s\n", token);
+            printf("Magnetic declination direction, E (east) or W (west):\t%s\n", token);
             break;
         case 12:
-            printf("Mode indication (A=autonomous positioning, D=differential, E=estimation, N=invalid data)\n* Statement end marker\nxx XOR check value of all bytes starting from $ to *\n<CR> Carriage return, end tag\n<LF> line feed, end tag: %s\n", token);
+            printf("Mode indication (A=autonomous positioning, D=differential, E=estimation, N=invalid data)\n* Statement end marker\nxx XOR check value of all bytes starting from $ to *\n<CR> Carriage return, end tag\n<LF> line feed, end tag:\t%s\n", token);
             break;
 
         default:
